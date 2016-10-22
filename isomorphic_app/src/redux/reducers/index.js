2443ea85ced5b255377fc297a030bcaf0f676b17
Alexey Levanov <ilewanof@gmail.com>
2016-10-23 00:26:45 +0400

    added action, reducer for timePage component

--- isomorphic_app/src/redux/reducers/index.js
+++ isomorphic_app/src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import counterReducer from './counterReducer';
+import timeReducer from './timerReducer';
 
 export default combineReducers({
-  counter: counterReducer
+  counter: counterReducer,
+  time: timeReducer
 });
