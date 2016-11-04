9e371c09cdd211200060f483c1e4c552b3850070
Alexey Levanov <ilewanof@gmail.com>
2016-11-04 16:19:24 +0400

    added mutation and createUser

--- hapi_graphql/graphql/resolvers.js
+++ hapi_graphql/graphql/resolvers.js
@@ -8,6 +8,12 @@ const resolvers = (models) => ({
       return models.User.findOne({ email }).then((response) => response);
     },
   },
+  Mutation: {
+    createUser(root, args) {
+      const user = new models.User(args);
+      return user.save().then((response) => response);
+    },
+  },
 });
 
 module.exports = resolvers;
