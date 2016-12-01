f0ba72e6f586aab9f29d6c89f84e97b79097a1c3
Alexey Levanov <ilewanof@gmail.com>
2016-12-02 00:35:57 +0400

    initial project

---
+++ vk_music_app/src/containers/Authorize/Authorize.js
@@ -0,0 +1,28 @@
+import React, { Component, PropTypes } from 'react';
+
+import Scrollable from '../../components/Scrollable/Scrollable';
+import Button from '../../components/Button/Button';
+
+import classes from './authorize.scss';
+
+class Authorize extends Component {
+  static propTypes = {
+    authorized: PropTypes.bool.isRequired,
+    authorize: PropTypes.func.isRequired
+  };
+
+  render() {
+    return (
+      <Scrollable>
+        <div className={ classes.component }>
+          <h2 className={ classes.title } data-text="VK Music">VK Music</h2>
+          <Button className={ classes.button } ripple={ true } onClick={ this.props.authorize }>
+            <span>Авторизоваться</span>
+          </Button>
+        </div>
+      </Scrollable>
+    )
+  }
+}
+
+export default Authorize;
---
+++ vk_music_app/src/router/index.js
@@ -0,0 +1,48 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { Router } from 'react-router';
+
+import App from '../containers/App/App';
+import Authorize from '../containers/Authorize/Authorize';
+
+class MyRouter extends Component {
+  routes = {
+    path: '/',
+    indexRoute: { onEnter: (nextState, replace) => this.onEnter(nextState, replace) },
+    childRoutes: [{
+      path: 'authorize',
+      component: Authorize,
+      onEnter: (nextState, replace) => this.onEnter(nextState, replace)
+    }]
+  }
+
+  onEnter(nextState, replace) {
+    const nextPathName = nextState.location.pathname;
+
+    if (!this.props.authorized && nextPathName !== '/authorize') {
+      replace({
+        pathname: '/authorize',
+        state: nextPathName
+      });
+    } else if (this.props.authorized && (nextPathName === '/' || nextPathName === '/authorize')) {
+      replace({
+        pathname: `/${this.props.userId}`,
+        state: nextPathName
+      });
+    }
+  }
+
+  render() {
+    return (
+      <Router history={ this.props.history } routes={ this.routes }/>
+    )
+  }
+}
+
+const mapStateToProps = ({ vk, routing }) => ({
+  authorized: vk.authorized,
+  userId: vk.userId,
+  routing: routing.locationBeforeTransitions
+});
+
+export default connect(mapStateToProps)(MyRouter);
